cabf6842feb6f606ba1222dce6a31c8d71b602d6
jcst2019 <juansolar_4@hotmail.com>
2025-02-18 06:35:55 -0500

    Configurar Routing con Lazy Loading y Preloading

--- modular-app/src/app/app-routing.module.ts
+++ modular-app/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'products', loadChildren: () => import('./features/products/products.module').then(m => m.ProductsModule) }, { path: 'users', loadChildren: () => import('./features/users/users.module').then(m => m.UsersModule) }];
+const routes: Routes = [
+  { path: 'products', loadChildren: () => import('./features/products/products.module').then(m => m.ProductsModule) }, 
+  { path: 'users', loadChildren: () => import('./features/users/users.module').then(m => m.UsersModule) }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{ preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
