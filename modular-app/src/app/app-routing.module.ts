d80d166be2f7e7acfc814a79a51d5e9db6f77842
jcst2019 <juansolar_4@hotmail.com>
2025-02-18 06:33:16 -0500

    crear module features products y users

--- modular-app/src/app/app-routing.module.ts
+++ modular-app/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'products', loadChildren: () => import('./features/products/products.module').then(m => m.ProductsModule) }, { path: 'users', loadChildren: () => import('./features/users/users.module').then(m => m.UsersModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
